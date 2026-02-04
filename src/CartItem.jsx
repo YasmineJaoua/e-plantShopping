8035272e5740c7f0935ba723b918e9832a4ff8e1
YasmineJaoua <yasmine.jaoua5526@gmail.com>
2026-02-04 11:37:20 -0500

    3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -18,16 +18,36 @@ const calculateTotalAmount = () => {
   return total;
 };
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
   const handleIncrement = (item) => {
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        quantity: item.quantity + 1,
+      })
+    );
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({
+          name: item.name,
+          quantity: item.quantity - 1,
+        })
+      );
+    } else {
+      dispatch(
+        removeItem({
+          name: item.name,
+        })
+      );
+    }
   };
 
   const handleRemove = (item) => {
@@ -35,6 +55,7 @@ const calculateTotalAmount = () => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
